a2d1a392ef0e36cacba4aff64ef41c489a650f80
anandgonaboyina <anandgonaboyina@gmail.com>
2026-03-27 21:45:32 +0530

    Update axiosInstance.js

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import toast from "react-hot-toast"
 
-const API = axios.create({baseURL : "http://localhost:4400"});
+const API = axios.create({baseURL : import.meta.env.VITE_API_URL || "http://localhost:4400/api",
+    withCredentials: true});
 
 API.interceptors.request.use(
     (req)=>
